c265320b6fb5e2f7c423989d98779884eb227178
Sofia-flower <kovalyshyn.sofiia@gmail.com>
2025-02-27 19:00:28 +0200

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,57 @@
+# def fist_fuction() :
+#     variable1 = "Hello"
+#     return variable1
+# print(fist_fuction)
+# print(fist_fuction())
+
+# def second_function(arg_1, par_2):
+#     return arg_1+par_2
+# print(second_function)
+# print(second_function("Hello", "World"))
+# print(second_function(input(), input()))
+
+# def third_fuction(var_1, var_2, var_3):
+#     return var_1*1 + var_2/4 + var_3 **4.3
+# res_1 = third_fuction(1, 2, 3)
+# print(res_1)
+#
+# res_2 = third_fuction(var_1=1, var_2=2, var_3=3)
+# print(res_2)
+#
+# res_3 = third_fuction(1, 2, var_3=3)
+# print(res_3)
+
+# from random import randint
+# def coin_simulator():
+#     coin = randint(0, 1)
+#     if coin == 0:
+#         print("Orel")
+#     else:
+#         print("Reshka")
+# coin_simulator()
+
+# 1
+def fourth_fuction():
+    variable1 = "Hello World"
+    return variable1
+print(fourth_fuction())
+
+# 2
+def add_numbers():
+    a = int(input("Введіть число: "))
+    b = int(input("Введіть число:"))
+    return a + b
+sum = add_numbers()
+print("Сума чисел:", sum)
+
+# 3
+def rectangle():
+    a = int(input("Введіть ширину прямокутника:"))
+    b = int(input("Введіть висоту прямокутника:"))
+    return a * b
+area = rectangle()
+print("Площа прямокутника:", area)
+
+
+
+
